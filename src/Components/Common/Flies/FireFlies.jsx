28ec3c334299f6267aefa76bfa40c6796d69781c
1M0NsR3P0 <naimonchy@gmail.com>
2023-07-02 23:05:57 +0600

    darkmode added

---
+++ src/Components/Common/Flies/FireFlies.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Fly.css'
+
+const FireFlies = () => {
+    return (
+        <div className='overflow-hidden'>
+            <div className='effectedContainer'><div className='effect'></div></div>
+            <div className='effectedContainer2'><div className='effect'></div></div>
+        </div>
+    );
+};
+
+export default FireFlies;
--- src/Components/Pages/Home/Services.jsx
+++ src/Components/Pages/Home/Services.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { DarkModeContext } from '../../Utilities/DarkmodeContext';
+import FireFlies from '../../Common/Flies/FireFlies';
 
 const Services = () => {
+    const {darkMode} = useContext(DarkModeContext)
     return (
         <div>
-            <div className='flex justify-center items-center h-[80vh] text-black'>
+            <div className={`overflow-hidden flex justify-center items-center h-[80vh] ${!darkMode?'text-white':'text-black'}`}>
                 No services Available now...
+                <FireFlies/>
             </div>
            
         </div>
